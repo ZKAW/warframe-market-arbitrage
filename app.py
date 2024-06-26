4b3b6e065d10b0329246f4b8a1ab447f5c711335
ZKAW <zkaw.exod@gmail.com>
2024-06-26 23:39:02 +0200

    feat: take into account parts quantity during price calculation

--- app.py
+++ app.py
@@ -17,16 +17,33 @@ def find_related_parts(set_name, all_items):
     base_name = set_name.replace('_set', '')
     return [item['url_name'] for item in all_items if item['url_name'].startswith(base_name) and not item['url_name'].endswith('_set')]
 
-def fetch_item_price(item_name):
+def get_set_info(set_name):
+    response = requests.get(f'https://api.warframe.market/v1/items/{set_name}')
+    return response.json()['payload']['item']
+
+def extract_quantity_from_item(item_name, set_info):
+    if item_name.endswith('_set'):
+        return 1
+    for component in set_info['items_in_set']:
+        if component['url_name'] == item_name:
+            try:
+                return int(component['quantity_for_set'])
+            except (ValueError, KeyError):
+                return 1
+
+    return 1
+
+def fetch_item_details(item_name, set_info=None):
     # print("Fetching item price for:", item_name)
     response = requests.get(f'https://api.warframe.market/v1/items/{item_name}/orders')
+    quantity = 1
     if response.status_code == 404:
         return None
     # if the error is a rate limit error, wait for a few seconds and try again
     if response.status_code != 200:
         print("Rate limited. Waiting for 1 second...")
         time.sleep(1)
-        return fetch_item_price(item_name)
+        return fetch_item_details(item_name)
     orders = response.json()['payload']['orders']
     prices = [
         order['platinum'] for order in orders
@@ -35,7 +52,47 @@ def fetch_item_price(item_name):
         and order['platform'] == 'pc'
         and order['user']['status'] == 'ingame'
     ]
-    return min(prices) if prices else None
+
+    if set_info:
+        quantity = extract_quantity_from_item(item_name, set_info)
+
+    price = min(prices) if prices else None
+
+    if price is None:
+        return None
+
+    return {
+        'price': price,
+        'quantity': quantity
+    }
+
+def fetch_set_price(set_name):
+    set_data = fetch_item_details(set_name)
+    time.sleep(0.1)
+
+    if set_data is None or not set_data['price']:
+        return None
+
+    try:
+        set_price = set_data['price']
+    except KeyError:
+        print(f"Failed to fetch price for {set_name}. Skipping...")
+        return None
+
+    return set_price
+
+def fetch_part_prices(set_name, set_info, all_items):
+    part_names = find_related_parts(set_name, all_items)
+    part_prices = []
+    for part_name in part_names:
+        item = fetch_item_details(part_name, set_info)
+        time.sleep(0.2)
+        if item is None or not item:
+            part_prices = [None]
+            break
+        part_prices.append(item['price'] * item['quantity'])
+
+    return part_prices
 
 def find_arbitrage_opportunities(sets, all_items):
     opportunities = []
@@ -43,30 +100,19 @@ def find_arbitrage_opportunities(sets, all_items):
 
     for set_item in sets:
         set_name = set_item['url_name']
-        set_price = fetch_item_price(set_name)
-        time.sleep(0.1)
+        set_info = get_set_info(set_name)
 
-        if set_price is None or not set_price:
+        set_price = fetch_set_price(set_name)
+        if set_price is None:
             continue
 
         print(f"Checking arbitrage opportunities for {set_name}...")
-        part_names = find_related_parts(set_name, all_items)
-        # print(f"Related parts for {set_name}: {part_names}")
-        part_prices = []
-        for part_name in part_names:
-            price = fetch_item_price(part_name)
-            time.sleep(0.2)
-            if price is None:
-                part_prices = [None]
-                break
-            # print(f"Price for {part_name}: {price}")
-            part_prices.append(price)
+        part_prices = fetch_part_prices(set_name, set_info, all_items)
 
         if None in part_prices:
             print(f"Failed to fetch prices for {set_name}. Skipping...")
             continue
 
-        # print(f"{set_name}: Set price: {set_price}, Part prices: {part_prices}")
         total_part_price = sum(part_prices)
         if total_part_price == 0:
             continue
