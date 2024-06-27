945cccd006cec2de2549a09ddb5a8c2007c08613
ZKAW <zkaw.exod@gmail.com>
2024-06-27 17:32:32 +0200

    feat: safety and volume param

--- app.py
+++ app.py
@@ -3,12 +3,41 @@ import time
 import json
 
 def read_config():
-    with open('config.json') as f:
-        return json.load(f)
+    if not hasattr(read_config, "_config_data"):
+        with open('config.json') as f:
+            read_config._config_data = json.load(f)
+    return read_config._config_data
+
+def safe_get_request(url, params=None, retries=5):
+    response = requests.get(url, params=params)
+    time.sleep(read_config()['REQUEST_DELAY'])
+    if response.status_code == 200:
+        return response
+    if response.status_code == 404:
+        return None
+    if retries == 0:
+        print(f"Retries exhausted for {url}. Skipping...")
+        return None
+
+    delay = read_config()['RATE_LIMIT_DELAY']
+    print("Rate limited. Waiting for {delay} seconds...")
+    time.sleep(delay)
+    return safe_get_request(url, params, retries - 1)
+
+def safe_get_keys(data, *keys, default=None):
+    try:
+        for key in keys:
+            data = data[key]
+        return data
+    except (KeyError, IndexError, TypeError):
+        return default
 
 def fetch_all_items():
-    response = requests.get('https://api.warframe.market/v1/items')
-    return response.json()['payload']['items']
+    response = safe_get_request('https://api.warframe.market/v1/items')
+    if response is None:
+        return None
+
+    return safe_get_keys(response.json(), 'payload', 'items')
 
 def find_eligible_sets(items):
     return [item for item in items if item['url_name'].endswith('_set')]
@@ -17,19 +46,19 @@ def find_related_parts(set_name, all_items):
     base_name = set_name.replace('_set', '')
     return [item['url_name'] for item in all_items if item['url_name'].startswith(base_name) and not item['url_name'].endswith('_set')]
 
-def get_set_info(set_name):
-    response = requests.get(f'https://api.warframe.market/v1/items/{set_name}')
-    if response.status_code == 404:
+def get_volume(item_name):
+    response = safe_get_request(f'https://api.warframe.market/v1/items/{item_name}/statistics')
+    if response is None:
         return None
-    if response.status_code != 200:
-        print("Rate limited. Waiting for 1 second...")
-        time.sleep(1)
-        return get_set_info(set_name)
 
-    try:
-        return response.json()['payload']['item']
-    except KeyError:
+    return safe_get_keys(response.json(), 'payload', 'statistics_closed', '48hours', -1, 'volume')
+
+def get_set_info(set_name):
+    response = safe_get_request(f'https://api.warframe.market/v1/items/{set_name}')
+    if response is None:
         return None
+
+    return safe_get_keys(response.json(), 'payload', 'item')
 
 def extract_quantity_from_item(item_name, set_info):
     if item_name.endswith('_set'):
@@ -44,17 +73,15 @@ def extract_quantity_from_item(item_name, set_info):
     return 1
 
 def fetch_item_details(item_name, set_info=None):
-    # print("Fetching item price for:", item_name)
-    response = requests.get(f'https://api.warframe.market/v1/items/{item_name}/orders')
     quantity = 1
-    if response.status_code == 404:
+    response = safe_get_request(f'https://api.warframe.market/v1/items/{item_name}/orders')
+    if response is None:
         return None
-    # if the error is a rate limit error, wait for a few seconds and try again
-    if response.status_code != 200:
-        print("Rate limited. Waiting for 1 second...")
-        time.sleep(1)
-        return fetch_item_details(item_name)
-    orders = response.json()['payload']['orders']
+
+    orders = safe_get_keys(response.json(), 'payload', 'orders')
+    if orders is None:
+        return None
+
     prices = [
         order['platinum'] for order in orders
         if order['order_type'] == 'sell'
@@ -78,14 +105,13 @@ def fetch_item_details(item_name, set_info=None):
 
 def fetch_set_price(set_name):
     set_data = fetch_item_details(set_name)
-    time.sleep(0.1)
 
     if set_data is None or not set_data['price']:
         return None
 
     try:
         set_price = set_data['price']
-    except KeyError:
+    except (KeyError, IndexError):
         print(f"Failed to fetch price for {set_name}. Skipping...")
         return None
 
@@ -96,7 +122,6 @@ def fetch_part_prices(set_name, set_info, all_items):
     part_prices = []
     for part_name in part_names:
         item = fetch_item_details(part_name, set_info)
-        time.sleep(0.3)
         if item is None or not item:
             part_prices = [None]
             break
@@ -107,9 +132,16 @@ def fetch_part_prices(set_name, set_info, all_items):
 def find_arbitrage_opportunities(sets, all_items):
     opportunities = []
     MIN_ARBITRAGE_VALUE = read_config()['MIN_ARBITRAGE_VALUE']
+    MIN_VOLUME = read_config()['MIN_VOLUME']
 
     for set_item in sets:
         set_name = set_item['url_name']
+        set_volume = get_volume(set_name)
+        if set_volume is None or set_volume < MIN_VOLUME:
+            print(f"Volume for {set_name} is too low", end="")
+            if set_volume is not None:
+                print(f" (was {set_volume}, required {MIN_VOLUME}). Skipping...")
+            continue
         set_info = get_set_info(set_name)
 
         set_price = fetch_set_price(set_name)
